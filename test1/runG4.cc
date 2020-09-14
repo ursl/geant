400abf95fd97744aef4d15eb9419422bd0f50835
Urs Langenegger <urslangenegger@gmail.com>
2020-09-14 10:23:27 +0200

    minor changes

--- test1/runG4.cc
+++ test1/runG4.cc
@@ -1,33 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-// $Id: exampleB2a.cc 86065 2014-11-07 08:51:15Z gcosmo $
-//
-/// \file exampleB2a.cc
-/// \brief Main program of the B2a example
-
 #include "DetectorConstruction.hh"
 
 #ifdef G4MULTITHREADED
@@ -51,64 +21,50 @@
 #include "RunAction.hh"
 #include "EventAction.hh"
 
+#include "RootIO.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-int main(int argc,char** argv)
-{
-  // Detect interactive mode (if no arguments) and define UI session
-  //
+// ----------------------------------------------------------------------
+int main(int argc,char** argv) {
+  // -- Detect interactive mode (if no arguments) and define UI session
   G4UIExecutive* ui = 0;
   if ( argc == 1 ) {
     ui = new G4UIExecutive(argc, argv);
   }
 
-  // Choose the Random engine
+  // -- Choose the Random engine
   G4Random::setTheEngine(new CLHEP::RanecuEngine);
 
-  // Construct the default run manager
-  //
+  // -- Default run manager
 #ifdef G4MULTITHREADED
   G4MTRunManager* runManager = new G4MTRunManager;
 #else
   G4RunManager* runManager = new G4RunManager;
 #endif
 
-  // Set mandatory initialization classes
-  //
-  //ul  runManager->SetUserInitialization(new DetectorConstruction());
-
-  // UserInitialization classes (mandatory)
+  // -- UserInitialization classes (mandatory)
   DetectorConstruction* detector = new DetectorConstruction;
   runManager->SetUserInitialization(detector);
-
 
   G4VModularPhysicsList* physicsList = new FTFP_BERT;
   physicsList->RegisterPhysics(new G4StepLimiterPhysics());
   runManager->SetUserInitialization(physicsList);
 
-  // Set user action classes
-  //  runManager->SetUserInitialization(new ActionInitialization());
-
-  // UserAction classes
+  // -- Set user action classes
   runManager->SetUserAction(new PrimaryGeneratorAction(detector));
   runManager->SetUserAction(new RunAction);
   runManager->SetUserAction(new EventAction);
 
   runManager->Initialize();
 
-  // Initialize visualization
-  //
+  // -- Initialize visualization
   G4VisManager* visManager = new G4VisExecutive;
-  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
-  // G4VisManager* visManager = new G4VisExecutive("Quiet");
   visManager->Initialize();
 
-  // Get the pointer to the User Interface manager
+  // --  Pointer to the User Interface manager
   G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
-  // Process macro or start UI session
-  //
+  // -- Process macro or start UI session
   if ( ! ui ) {
     // batch mode
     G4String command = "/control/execute ";
@@ -125,15 +81,10 @@ int main(int argc,char** argv)
     delete ui;
   }
 
-  // Job termination
-  // Free the store: user actions, physics_list and detector_description are
-  // owned and deleted by the run manager, so they should not be deleted
-  // in the main() program !
-  //
+  RootIO::GetInstance()->Close();
+
   delete visManager;
   delete runManager;
 
   return 0;
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
