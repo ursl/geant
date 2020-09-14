805ca036da813529bd96966439e9ff6330186e63
Urs Langenegger <urslangenegger@gmail.com>
2020-09-14 10:22:03 +0200

    go to class persistency

--- test1/src/EventAction.cc
+++ test1/src/EventAction.cc
@@ -6,11 +6,14 @@
 #include "G4Trajectory.hh"
 #include "G4ios.hh"
 
+#include "RootIO.hh"
+
 // ----------------------------------------------------------------------
 EventAction::EventAction(): G4UserEventAction() { }
 
 // ----------------------------------------------------------------------
-EventAction::~EventAction() { }
+EventAction::~EventAction() {
+}
 
 // ----------------------------------------------------------------------
 void EventAction::BeginOfEventAction(const G4Event*) { }
@@ -25,9 +28,40 @@ void EventAction::EndOfEventAction(const G4Event* evt) {
   G4int n_trajectories = 0;
   if (trajectoryContainer) n_trajectories = trajectoryContainer->entries();
 
-  if (event_id < 100 || event_id%100 == 0) {
-    G4cout << ">>> Event " << evt->GetEventID() << G4endl;
-    G4cout << "    " << n_trajectories
-           << " trajectories stored in this event." << G4endl;
+  RootIO *rio = RootIO::GetInstance();
+  std::vector<int> genlist;
+  G4cout << "----------------------------------------------------------------------" << G4endl;
+  for (G4int i = 0; i < n_trajectories; ++i) {
+    G4Trajectory* trj=(G4Trajectory*)((*(evt->GetTrajectoryContainer()))[i]);
+    if (1 == trj->GetParentID()) {
+      genlist.push_back(i);
+    }
+    G4cout << " Evt/trj =  " << event_id << "/" << i << " " << trj->GetParticleName()
+	   << " ID/Mum " << trj->GetTrackID() << "/" << trj->GetParentID()
+	   << " charge: " << trj->GetCharge()
+	   << " energy: " << trj->GetInitialKineticEnergy()
+	   <<  G4endl;
   }
+
+  G4cout << "----------------------------------------------------------------------" << G4endl;
+  G4cout << "genlist size() = " << genlist.size() << G4endl;
+
+  double px, py, pz, e;
+  int id;
+  for (unsigned int i = 0; i < genlist.size(); ++i) {
+    G4Trajectory* trj=(G4Trajectory*)((*(evt->GetTrajectoryContainer()))[genlist[i]]);
+    px = trj->GetInitialMomentum().x();
+    py = trj->GetInitialMomentum().y();
+    pz = trj->GetInitialMomentum().z();
+    e  = trj->GetInitialKineticEnergy();
+    id = trj->GetPDGEncoding();
+    G4cout << "(p,e) = " << px << "/" << py << "/" << pz << "/" << e << " ID = " << id << G4endl;
+    // rio->fGenParticles[i].px = px;
+    // rio->fGenParticles[i].py = py;
+    // rio->fGenParticles[i].pz = pz;
+    // rio->fGenParticles[i].e = e;
+    // rio->fGenParticles[i].id = id;
+  }
+  rio->fNGenParticles = genlist.size();
+  rio->fillTree();
 }
