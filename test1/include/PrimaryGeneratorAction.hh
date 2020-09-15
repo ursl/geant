369dd5e070fd022b926058755a62ff4d7edd2148
Urs Langenegger <urslangenegger@gmail.com>
2020-09-15 13:25:40 +0200

    changed to GPS, but bbody not yet working

--- test1/include/PrimaryGeneratorAction.hh
+++ test1/include/PrimaryGeneratorAction.hh
@@ -1,65 +1,23 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-/// \file persistency/P01/include/PrimaryGeneratorAction.hh
-/// \brief Definition of the PrimaryGeneratorAction class
-//
-//
-// $Id: PrimaryGeneratorAction.hh 98770 2016-08-09 14:22:25Z gcosmo $
-//
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
- 
 #ifndef PrimaryGeneratorAction_h
 #define PrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
 
 class DetectorConstruction;
-class G4ParticleGun;
+class G4GeneralParticleSource;
 class G4Event;
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
+public:
+  PrimaryGeneratorAction(DetectorConstruction*);
+  ~PrimaryGeneratorAction();
 
-/// Primary generator action for the persistency example
- 
-class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
-{
-  public:
-    PrimaryGeneratorAction(DetectorConstruction*);    
-   ~PrimaryGeneratorAction();
+public:
+  virtual void GeneratePrimaries(G4Event*);
 
-  public:
-    virtual void GeneratePrimaries(G4Event*);
-
-  private:
-    G4ParticleGun* fParticleGun;
-    DetectorConstruction* fMyDetector;
+private:
+  G4GeneralParticleSource* fParticleGun;
+  DetectorConstruction* fMyDetector;
 };
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #endif
-
