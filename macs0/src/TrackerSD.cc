1631e3bad34334903daa54ac29de3470c30d508c
Urs Langenegger <urslangenegger@gmail.com>
2020-10-30 11:24:59 +0100

    nada

--- macs0/src/TrackerSD.cc
+++ macs0/src/TrackerSD.cc
@@ -11,7 +11,7 @@
 TrackerSD::TrackerSD(const G4String& name, const G4String& hitsCollectionName) :
   G4VSensitiveDetector(name),
   fHitsCollection(NULL) {
-  collectionName.insert(name);
+  collectionName.insert(hitsCollectionName);
 }
 
 // ----------------------------------------------------------------------
@@ -21,26 +21,24 @@ TrackerSD::~TrackerSD() {
 
 // ----------------------------------------------------------------------
 void TrackerSD::Initialize(G4HCofThisEvent* hce) {
-  fHitsCollection = new TrackerHitsCollection(SensitiveDetectorName, collectionName[0]);
-
-  // Add this collection in hce
+  fHitsCollection = new TrackerHitsCollection(GetName(), collectionName[0]);
   G4int hcID  = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
-  hce->AddHitsCollection( hcID, fHitsCollection );
+  hce->AddHitsCollection(hcID, fHitsCollection);
 }
 
 // ----------------------------------------------------------------------
 G4bool TrackerSD::ProcessHits(G4Step* aStep, G4TouchableHistory*) {
   G4double edep = aStep->GetTotalEnergyDeposit();
 
-  if(edep==0.) return false;
+  if (edep==0.) return false;
 
-  TrackerHit* newHit = new TrackerHit();
+  if (0) G4cout << "==========> TrackerSD::ProcessHits> new hit added " << G4endl;
+  TrackerHit *newHit = new TrackerHit();
   newHit->SetTrackID  (aStep->GetTrack()->GetTrackID());
-  newHit->SetChamberNb(aStep->GetPreStepPoint()->GetTouchable()
-                                               ->GetReplicaNumber());
+  newHit->SetChamberNb(aStep->GetPreStepPoint()->GetTouchable()->GetReplicaNumber());
   newHit->SetEdep     (edep);
   newHit->SetPos      (aStep->GetPostStepPoint()->GetPosition());
-  fHitsCollection->insert( newHit );
+  fHitsCollection->insert(newHit);
 
   return true;
 }
@@ -51,15 +49,12 @@ void TrackerSD::EndOfEvent(G4HCofThisEvent*) {
   G4int NbHits = fHitsCollection->entries();
   std::vector<TrackerHit*> hitsVector;
 
-  {
+  if (1) {
     G4cout << "\n-------->Storing hits in the ROOT file: in this event there are " << NbHits
            << " hits in the tracker chambers: " << G4endl;
     for (G4int i=0;i<NbHits;i++) (*fHitsCollection)[i]->Print();
   }
 
-
-  for (G4int i=0;i<NbHits;i++)
-    hitsVector.push_back((*fHitsCollection)[i]);
-
+  for (G4int i=0;i<NbHits;i++) hitsVector.push_back((*fHitsCollection)[i]);
   RootIO::GetInstance()->Write(&hitsVector);
 }
