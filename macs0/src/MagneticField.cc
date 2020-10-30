2b8cbfdd10e770ae6575c0da25b28c17ced7cff4
Urs Langenegger <urslangenegger@gmail.com>
2020-10-30 11:26:02 +0100

    dbx; change messenger cmd:

--- macs0/src/MagneticField.cc
+++ macs0/src/MagneticField.cc
@@ -13,19 +13,27 @@ MagneticField::~MagneticField() {
   delete fMessenger;
 }
 
+// ----------------------------------------------------------------------
+void MagneticField::SetField(G4double val) {
+  G4cout << "=========> MagneticField::SetField(" << val << ")!" << G4endl;
+  fBy = val;
+}
+
+
 
 // ----------------------------------------------------------------------
 void MagneticField::GetFieldValue(const G4double [4], double *bField) const {
   bField[0] = 0.;
   bField[1] = fBy;
   bField[2] = 0.;
+  //  G4cout << "=========> MagneticField::GetFieldValue() called, fBy = " << fBy  << G4endl;
 }
 
 
 // ----------------------------------------------------------------------
 void MagneticField::DefineCommands() {
   // Define /B5/field command directory using generic messenger class
-  fMessenger = new G4GenericMessenger(this, "/muamu/field/", "Field control");
+  fMessenger = new G4GenericMessenger(this, "/macs0/field/", "Field control");
 
   // fieldValue command
   auto& valueCmd = fMessenger->DeclareMethodWithUnit("value","tesla", &MagneticField::SetField, "Set field strength.");
